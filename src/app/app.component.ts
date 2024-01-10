4585a08fec3ddc3c9ac7d658a824fe3a7a17e1bb
arunG2023 <arun.g@vsaasglobal.com>
2024-01-10 09:58:39 +0530

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'empty';
+
+  form! : FormGroup;
+
+  constructor(){
+    this.form = new FormGroup({
+      name : new FormControl(''),
+      age : new FormControl(''),
+      file1 : new FormControl(''),
+      file2 : new FormControl(''),
+      file3 : new FormControl('')
+    })
+  }
+
+  submit(){
+    console.log(this.form)
+  }
+
 }
